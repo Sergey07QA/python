668c9772ed0200858f9e7a9e060a78bd6d313d39
Sergey07QA <Karnauhov-sv@yandex.ru>
2025-08-20 18:17:53 +0300

    Add files via upload

--- Project/main.py
+++ Project/main.py
@@ -3,7 +3,7 @@ import requests
 url = 'https://api.pokemonbattle.ru/v2'
 
 token = '667556ad8ccbdb642ced81f4b4e6eee0'
-header= {'Content-Type': 'application/json', 'trainer_token':token}
+header = {'Content-Type': 'application/json', 'trainer_token':token}
 body_registration = {
     "trainer_token": token,
     "email": "Karnauhov-sv@yandex.ru",
@@ -21,14 +21,14 @@ body_add_pokeballs = {
 
 
 
-'''response = requests.post(url=f'{url}/trainers/reg', headers=header, json=body_registration)
+'''response = requests.post(url = f'{url}/trainers/reg', headers = header, json = body_registration)
 print(response.text)'''
 
-response_create =requests.post(url=f'{url}/pokemons', headers=header, json = body_create)
+response_create = requests.post(url = f'{url}/pokemons', headers = header, json = body_create)
 print(response_create.json())
 
 message = response_create.json()['message']
 print(message)
 
-response_add_pokeball =requests.post(url=f'{url}/trainers/add_pokeball', headers=header, json = body_add_pokeballs)
+response_add_pokeball = requests.post(url = f'{url}/trainers/add_pokeball', headers = header, json = body_add_pokeballs)
 print(response_add_pokeball.text)
--- Project/tests/test_homework.py
+++ Project/tests/test_homework.py
@@ -3,15 +3,15 @@ import pytest
 
 url = 'https://api.pokemonbattle.ru/v2'
 token = '667556ad8ccbdb642ced81f4b4e6eee0'
-header= {'Content-Type': 'application/json', 'trainer_token':token}
+header = {'Content-Type': 'application/json', 'trainer_token':token}
 trainer_id = '38442'
 
 
 def test_status_code():
-    response = requests.get(url=f'{url}/pokemons', params={'trainer_id': trainer_id})
-    assert response.status_code==200
+    response = requests.get(url = f'{url}/pokemons', params = {'trainer_id': trainer_id})
+    assert response.status_code == 200
 
 
 def test_part_of_response():
-    response_get = requests.get(url=f'{url}/pokemons', params={'trainer_id': trainer_id})
+    response_get = requests.get(url = f'{url}/pokemons', params = {'trainer_id': trainer_id})
     assert response_get.json()['data'][0]['name'] == 'Бульбазавр'
